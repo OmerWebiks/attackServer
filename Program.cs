7909f97406da89b265ca34f3b63fdc7322d1450e
OmerWebiks <omer_munk@webiks.com>
2024-08-06 10:44:53 +0300

    instructions

--- Program.cs
+++ Program.cs
@@ -8,8 +8,21 @@ namespace attackServer
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+          string result = await GetDataAsync();
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+        public static async Task<string> GetDataAsync()
+        {
+            await Task.Delay(2000);
+            return "Recivde Data";
+        }
+
+        public static async Task<string> ReadFileAsync() { 
+            //file.ReadAllText
+            //Task.run()
         }
     }
 }
