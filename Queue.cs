71eaf317f14f08e11cf42398ef3653a82fcce849
OmerWebiks <omer_munk@webiks.com>
2024-08-06 12:43:28 +0300

    implement queue and enqueue

---
+++ Queue.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace attackServer
+{
+    internal class Queue
+    {
+        private Node _head;
+        private Node _tail;
+        private int _count;
+
+        public Queue()
+        {
+            this.setHead(null);
+            this.setTail(null);
+            this.setCount(0);
+        }
+
+        // (3, newNode)
+        // (4, null)
+
+        public void Enqueue(int value) { 
+            Node newNode = new Node(value);
+            if (this.getTail() != null) { 
+                Node tail = this.getTail();
+                tail.setNext(newNode);
+            }
+            if (this.getHead() == null) { 
+                this.setHead(newNode);
+                
+            }
+            this.setTail(newNode);
+            this.setCount(this.getCount() + 1);
+        }
+
+        public void setHead(Node head) { 
+            this._head = head;
+        }
+
+        public void setTail(Node tail)
+        {
+            this._tail = tail;
+        }
+
+        public void setCount(int count)
+        {
+            this._count = count;
+        }
+
+        public Node getHead()
+        {
+            return this._head;
+        }
+
+        public Node getTail()
+        {
+            return this._tail;
+        }
+
+        public int getCount()
+        {
+            return this._count;
+        }
+    }
+
+
+
+
+}
